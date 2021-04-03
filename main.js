361f000992fa477352b6802f63cc9cf7c61f6dc6
PoisonusPanther <78292850+PoisonusPanther@users.noreply.github.com>
2021-04-03 13:54:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+}
+function modelLoaded(){
+    console.log("Model Loaded!");
+}
+function draw(){
+}
+
